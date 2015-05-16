240aa02544bc679d3486245ff526f0a65a062559
Héctor Molinero Fernández <me@znt.se>
2015-05-16 11:44:53 +0200

    Use tabs instead of spaces

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #
 
 #############################################
-# Globals:                                  #
+# Globals:									#
 #############################################
 
 SCRIPTDIR=$(dirname $0)
@@ -13,68 +13,68 @@ SCRIPTDIR=$(dirname $0)
 set -e
 
 #############################################
-# Messages:                                 #
+# Messages:									#
 #############################################
 
 function infoMsg {
-    echo -e "\e[1;33m + \e[1;32m$1 \e[0m"
+	echo -e "\e[1;33m + \e[1;32m$1 \e[0m"
 }
 
 function promptMsg {
-    echo -en "\e[1;33m + \e[1;32m$1 \e[0m"
-    read -p "[y/N]: " USER_RESPONSE
+	echo -en "\e[1;33m + \e[1;32m$1 \e[0m"
+	read -p "[y/N]: " USER_RESPONSE
 
-    if [[ $USER_RESPONSE =~ ^[Yy]$ ]]; then
-        return 0
-    else
-        return 1
-    fi
+	if [[ $USER_RESPONSE =~ ^[Yy]$ ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 #############################################
-# Actions:                                  #
+# Actions:									#
 #############################################
 
 function prerequisites {
-    if [ -d $HOME/.dropbox-dist ]; then
-        if promptMsg "This script has found a previous Dropbox installation. Do you want to continue?"; then
-            if [ "$(pidof dropbox)" ]; then
-                killall dropbox
-            fi
+	if [ -d $HOME/.dropbox-dist ]; then
+		if promptMsg "This script has found a previous Dropbox installation. Do you want to continue?"; then
+			if [ "$(pidof dropbox)" ]; then
+				killall dropbox
+			fi
 
-            rm -rf $HOME/.dropbox-dist
-        else
-            exit 0
-        fi
-    fi
+			rm -rf $HOME/.dropbox-dist
+		else
+			exit 0
+		fi
+	fi
 
-    if [ -d $HOME/.dropbox ]; then
-        rm -rf $HOME/.dropbox.bak
-        mv $HOME/.dropbox $HOME/.dropbox.bak
-    fi
+	if [ -d $HOME/.dropbox ]; then
+		rm -rf $HOME/.dropbox.bak
+		mv $HOME/.dropbox $HOME/.dropbox.bak
+	fi
 }
 
 function downloadDropbox {
-    infoMsg "Downloading package..."
+	infoMsg "Downloading package..."
 
-    if [ $(uname -m) == 'x86_64' ]; then
-        ARCH=x86_64
-    else
-        ARCH=x86
-    fi
+	if [ $(uname -m) == 'x86_64' ]; then
+		ARCH=x86_64
+	else
+		ARCH=x86
+	fi
 
-    wget -O - "https://www.dropbox.com/download?plat=lnx.$ARCH" --no-check-certificate | tar xzf - -C $HOME
+	wget -O - "https://www.dropbox.com/download?plat=lnx.$ARCH" --no-check-certificate | tar xzf - -C $HOME
 }
 
 function installIcons {
-    if promptMsg "Do you want to install the custom icons?"; then
-        mkdir -p $HOME/.local/share/icons/hicolor
-        cp -rT $SCRIPTDIR/icons $HOME/.local/share/icons/hicolor
-    fi
+	if promptMsg "Do you want to install the custom icons?"; then
+		mkdir -p $HOME/.local/share/icons/hicolor
+		cp -rT $SCRIPTDIR/icons $HOME/.local/share/icons/hicolor
+	fi
 }
 
 function createLaunchers {
-    cat > $SCRIPTDIR/dropbox.desktop <<-EOF
+	cat > $SCRIPTDIR/dropbox.desktop <<-EOF
 
 	[Desktop Entry]
 	Name=Dropbox
@@ -90,23 +90,23 @@ function createLaunchers {
 
 	EOF
 
-    infoMsg "Creating menu launcher..."
-    mkdir -p $HOME/.local/share/applications
-    cp $SCRIPTDIR/dropbox.desktop $HOME/.local/share/applications/
+	infoMsg "Creating menu launcher..."
+	mkdir -p $HOME/.local/share/applications
+	cp $SCRIPTDIR/dropbox.desktop $HOME/.local/share/applications/
 
-    infoMsg "Creating autostart launcher..."
-    mkdir -p $HOME/.config/autostart
-    cp $SCRIPTDIR/dropbox.desktop $HOME/.config/autostart/
+	infoMsg "Creating autostart launcher..."
+	mkdir -p $HOME/.config/autostart
+	cp $SCRIPTDIR/dropbox.desktop $HOME/.config/autostart/
 }
 
 function runDropbox {
-    if promptMsg "Run Dropbox now?"; then
-        XDG_CURRENT_DESKTOP=Unity $HOME/.dropbox-dist/dropboxd &
-    fi
+	if promptMsg "Run Dropbox now?"; then
+		XDG_CURRENT_DESKTOP=Unity $HOME/.dropbox-dist/dropboxd &
+	fi
 }
 
 #############################################
-# Process:                                  #
+# Process:									#
 #############################################
 
 prerequisites
--- uninstall.sh
+++ uninstall.sh
@@ -7,56 +7,56 @@
 set -e
 
 #############################################
-# Messages:                                 #
+# Messages:									#
 #############################################
 
 function infoMsg {
-    echo -e "\e[1;33m + \e[1;32m$1 \e[0m"
+	echo -e "\e[1;33m + \e[1;32m$1 \e[0m"
 }
 
 function promptMsg {
-    echo -en "\e[1;33m + \e[1;32m$1 \e[0m"
-    read -p "[y/N]: " USER_RESPONSE
+	echo -en "\e[1;33m + \e[1;32m$1 \e[0m"
+	read -p "[y/N]: " USER_RESPONSE
 
-    if [[ $USER_RESPONSE =~ ^[Yy]$ ]]; then
-        return 0
-    else
-        return 1
-    fi
+	if [[ $USER_RESPONSE =~ ^[Yy]$ ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 #############################################
-# Actions:                                #
+# Actions:									#
 #############################################
 
 function uninstallDropbox {
-    if promptMsg "Do you want to uninstall Dropbox?"; then
-        if [ "$(pidof dropbox)" ]; then
-            killall dropbox
-        fi
+	if promptMsg "Do you want to uninstall Dropbox?"; then
+		if [ "$(pidof dropbox)" ]; then
+			killall dropbox
+		fi
 
-        rm -rf \
-            $HOME/.dropbox-dist \
-            $HOME/.local/share/applications/dropbox.desktop \
-            $HOME/.config/autostart/dropbox.desktop
+		rm -rf \
+			$HOME/.dropbox-dist \
+			$HOME/.local/share/applications/dropbox.desktop \
+			$HOME/.config/autostart/dropbox.desktop
 
-        if [ -d $HOME/.dropbox ]; then
-            rm -rf $HOME/.dropbox.bak
-            mv $HOME/.dropbox $HOME/.dropbox.bak
-        fi
-    fi
+		if [ -d $HOME/.dropbox ]; then
+			rm -rf $HOME/.dropbox.bak
+			mv $HOME/.dropbox $HOME/.dropbox.bak
+		fi
+	fi
 }
 
 function uninstallIcons {
-    if promptMsg "Do you want to uninstall the custom icons?"; then
-        rm -rf \
-            $HOME/.local/share/icons/hicolor/*x*/apps/dropbox.* \
-            $HOME/.local/share/icons/hicolor/*x*/apps/dropboxstatus-{busy,busy2,idle,logo,x}.*
-    fi
+	if promptMsg "Do you want to uninstall the custom icons?"; then
+		rm -rf \
+			$HOME/.local/share/icons/hicolor/*x*/apps/dropbox.* \
+			$HOME/.local/share/icons/hicolor/*x*/apps/dropboxstatus-{busy,busy2,idle,logo,x}.*
+	fi
 }
 
 #############################################
-# Process:                                  #
+# Process:									#
 #############################################
 
 uninstallDropbox
